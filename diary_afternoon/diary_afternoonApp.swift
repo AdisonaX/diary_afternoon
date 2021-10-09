9b84e1ecfcf72b4a51439fc4aa2c1d6acb143eb2
Rui <rm02.business@gmail.com>
2021-10-09 12:10:20 +0900

    Initial Commit

---
+++ diary_afternoon/diary_afternoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  diary_afternoonApp.swift
+//  diary_afternoon
+//
+//  Created by Rui Miyashita on 2021/10/09.
+//
+
+import SwiftUI
+
+@main
+struct diary_afternoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
